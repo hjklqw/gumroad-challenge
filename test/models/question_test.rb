a8860ff3daa2fac7528713d9452a5c251dba617d
Michelle Poon <mp.hjklqw@gmail.com>
2023-06-08 03:52:15 -0600

    [Testing] - Added a test for the Question model

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without question" do
+    question = Question.new
+    assert_not(question.save, "Saved without a question")
+  end
 end
